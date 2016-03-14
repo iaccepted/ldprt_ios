dcad58eb9f3e65dacbaf509ca7d2f2769dbd4381
guohongzhi <succeed.2009@163.com>
2016-03-14 20:00:37 +0800

    Initial Commit

---
+++ ldprt_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ldprt_ios
+//
+//  Created by iaccepted on 16/3/14.
+//  Copyright (c) 2016年 iaccepted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ldprt_ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ldprt_ios
+//
+//  Created by iaccepted on 16/3/14.
+//  Copyright (c) 2016年 iaccepted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
